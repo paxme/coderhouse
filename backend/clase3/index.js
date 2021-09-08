927e69fcd2f6e6793ce29c904235b82193f5aa0a
Diego Alzate <diego35.da@gmail.com>
2021-09-07 20:55:38 -0500

    added callback clase3

--- backend/clase3/index.js
+++ backend/clase3/index.js
@@ -2,3 +2,17 @@
  // arrow function
 
  const max = (x,y) => Math.max(x,y)
+
+ const callback = (cb) => cb()
+
+ const log = () => console.log("Hello")
+
+//  callback(log)
+
+ const saludar = u => 'hola ' + u
+
+ const adios = u => 'adios ' + u
+
+ const crearSaludo = (cb, u) => cb(u)
+
+ console.log(crearSaludo(saludar, 'jorge'))
