8e7687055c6d4ae2299905ae43ab5f13f5fbc904
Diego Alzate <diego35.da@gmail.com>
2021-09-22 10:48:36 -0500

    deleting node modules

---
+++ clase6/crud/arr.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: "diego",
+        surname: "alzate",
+        id: 1
+    },
+    {
+        name: "jorgi",
+        surname: "rodriguez",
+        id: 2
+    },
+    {
+        name: "alejo",
+        surname: "andrade",
+        id: 3
+    },
+    {
+        name: "juan",
+        surname: "herreri",
+        id: 4
+    }
+]
---
+++ clase6/crud/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const arr = require('./arr')
+const app = express()
+const frase = "hola mundo como estan"
+app.use(express.json())
+  .use(express.urlencoded())
+
+// GET
+app.get("/users", (_, res) => {
+    res.json({
+        message: "users",
+        data: arr
+    })
+})
+
+app.get("/user/:id", (req, res) => {
+    res.json({
+        message: "found him",
+        data: arr.find(p => p.id === +req.params.id)
+    })
+})
+
+app.get("/api/frase", (req,res) => {
+    res.json({
+        statusCode: 200,
+        message: frase
+    })
+})
+
+// POST
+app.post("/user", (req, res) => {
+    console.log(req.body.body)
+    res.send("ok")
+})
+
+// DELETE
+
+
+// PUT
+
+app.listen(8080, () => {
+    console.log("Server is up on port 8080")
+})
