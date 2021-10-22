c0a51c9fa9e28c9bb7bb85bae0ded1bc88921880
Diego Alzate <diego35.da@gmail.com>
2021-10-22 00:28:17 -0500

    added more routes to carrito router

--- backend/clase13/desafio/src/index.js
+++ backend/clase13/desafio/src/index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 const productsRouter = require('./routes/products')
+const carritoRouter = require('./routes/carrito')
 
 app.use(express.json())
     .use(express.urlencoded({ extended: true }))
 
 app.use('/api/products', productsRouter)
+app.use('/api/carrito', carritoRouter)
 
 app.listen(PORT, () => {
     console.log("Port is up on " + PORT)
--- backend/clase13/desafio/src/routes/carrito.js
+++ backend/clase13/desafio/src/routes/carrito.js
@@ -2,5 +2,45 @@ const express = require('express')
 const { Router } = express
 const router = new Router()
 const { Contenedor } = require('../models/Contenedor')
-const c = new Contenedor('src/carrito.txt')
+const contenedorCarrito = new Contenedor('src/carrito.txt')
+const contenedorProductos = new Contenedor('src/productos.txt')
 
+// Post
+
+router.post('/', async (req, res) => {
+    const r = await contenedorCarrito.save({
+        timestamp: new Date.now()
+    })
+    res.json({
+        statusCode: 200,
+        body: `Created a cart with id: ${r}`
+    })
+})
+
+router.post('/:id/productos', (req, res) => {
+    
+})
+
+// Delete
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    await contenedorCarrito.deleteById(+id)
+    res.json({
+        statusCode: 200,
+        body: `Deleted a cart with the id: ${id}`
+    })
+})
+
+// Get
+
+router.get('/:id/productos', (req, res) => {
+    const carrito = await contenedorCarrito.getById(+req.params.id)
+    res.json({
+        statusCode: 200,
+        body: carrito
+    })
+})
+
+
+module.exports = router
