cc314b6ec4a1711ba202eb95e13a7decec3f8a1b
Diego Alzate <diego35.da@gmail.com>
2021-10-22 21:29:36 -0500

    added more cart router functionalities

--- backend/clase13/desafio/src/routes/carrito.js
+++ backend/clase13/desafio/src/routes/carrito.js
@@ -2,14 +2,15 @@ const express = require('express')
 const { Router } = express
 const router = new Router()
 const { Contenedor } = require('../models/Contenedor')
-const contenedorCarrito = new Contenedor('src/carrito.txt')
-const contenedorProductos = new Contenedor('src/productos.txt')
+const modelCart = new Contenedor('src/carrito.txt')
+const modelProducts = new Contenedor('src/products.txt')
 
 // Post
 
 router.post('/', async (req, res) => {
-    const r = await contenedorCarrito.save({
-        timestamp: new Date.now()
+    const r = await modelCart.save({
+        timestamp: Date.now(),
+        products: []
     })
     res.json({
         statusCode: 200,
@@ -17,28 +18,55 @@ router.post('/', async (req, res) => {
     })
 })
 
-router.post('/:id/productos', (req, res) => {
-    
+router.post('/:id/products', async (req, res) => {
+    const cartId = req.params.id
+    const cart = await modelCart.getById(+cartId)
+    const product = await modelProducts.getById(+req.body.id)
+    await modelCart.update({
+      ...cart,
+      products:
+        cart.products && cart.products.length
+          ? [...cart.products, product]
+          : [product],
+    })
+    res.json({
+        statusCode: 200,
+        body: `Created a cart with id: ${cartId}`
+    })
 })
 
 // Delete
 
 router.delete('/:id', async (req, res) => {
     const { id } = req.params
-    await contenedorCarrito.deleteById(+id)
+    await modelCart.deleteById(+id)
     res.json({
         statusCode: 200,
         body: `Deleted a cart with the id: ${id}`
     })
 })
 
-// Get
-
-router.get('/:id/productos', (req, res) => {
-    const carrito = await contenedorCarrito.getById(+req.params.id)
+router.delete('/:id/productos/:id_prod', async (req, res) => {
+    const { id: cartId, id_prod: productId } = req.params
+    const cart = await modelCart.getById(+cartId)
+    await modelCart.update({
+        ...cart,
+        products: [
+            ...cart.products.filter(product => product.id !== +productId)
+        ]
+    })
     res.json({
         statusCode: 200,
-        body: carrito
+        body: `Deleted a product with the id: ${productId}`
+    })
+})
+// Get
+
+router.get('/:id/products', async (req, res) => {
+    const carrito = await modelCart.getById(+req.params.id)
+    res.json({
+      statusCode: 200,
+      body: carrito.products && carrito.products.length ? carrito.products : [],
     })
 })
 
